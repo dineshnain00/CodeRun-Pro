68e1326eb39e92be73448c016c258214096bce58
DINESH NAIN <199170032+dineshnain00@users.noreply.github.com>
2025-10-06 15:15:52 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,22 @@
-// Node.js server code here
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+
+app.use(express.json());
+
+app.post('/run', (req, res) => {
+  const { language, code } = req.body;
+  const fileName = `temp.${language}`;
+  const fs = require('fs');
+  fs.writeFileSync(fileName, code);
+
+  exec(`docker run --rm -v $(pwd):/code mycompilerimage ./run_code.sh ${language} /code/${fileName}`, (err, stdout, stderr) => {
+    if (err) {
+      res.status(500).send(stderr);
+      return;
+    }
+    res.send(stdout);
+  });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
