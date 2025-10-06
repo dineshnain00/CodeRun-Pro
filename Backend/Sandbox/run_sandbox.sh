94535ceb64746bd9ef6090171bef2188113cc8f6
DINESH NAIN <199170032+dineshnain00@users.noreply.github.com>
2025-10-06 15:15:01 +0530

    Update run_sandbox.sh

--- Backend/Sandbox/run_sandbox.sh
+++ Backend/Sandbox/run_sandbox.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-# Sandbox execution script
+LANGUAGE=$1
+CODE_FILE=$2
+
+case $LANGUAGE in
+  python)
+    python3 $CODE_FILE
+    ;;
+  java)
+    javac $CODE_FILE && java ${CODE_FILE%.java}
+    ;;
+  node)
+    node $CODE_FILE
+    ;;
+  *)
+    echo "Unsupported language"
+    ;;
+esac
