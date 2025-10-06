f64cd68231a8bcd2b3cdc546f7a3275079c52b24
DINESH NAIN <199170032+dineshnain00@users.noreply.github.com>
2025-10-06 16:06:26 +0530

    Update languageConfig.js

--- shared/utiles/languageConfig.js
+++ shared/utiles/languageConfig.js
@@ -1,1 +1,14 @@
-// Language config mapping
+export const languageConfig = {
+  js: {
+    extension: "js",
+    run: (file) => `node ${file}`,
+  },
+  py: {
+    extension: "py",
+    run: (file) => `python3 ${file}`,
+  },
+  cpp: {
+    extension: "cpp",
+    run: (file) => `g++ ${file} -o /tmp/a.out && /tmp/a.out`,
+  },
+};
