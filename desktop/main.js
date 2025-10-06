7e78b204709718a267a149ca51ae2610fd29b2f8
DINESH NAIN <199170032+dineshnain00@users.noreply.github.com>
2025-10-06 15:19:56 +0530

    Update main.js

--- desktop/main.js
+++ desktop/main.js
@@ -1,1 +1,16 @@
-// Electron main process code
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, 'renderer/index.html'));
+}
+
+app.whenReady().then(createWindow);
