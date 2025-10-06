84b8a08c8bc81cdc00c8f23b57589a660547ac31
DINESH NAIN <199170032+dineshnain00@users.noreply.github.com>
2025-10-06 15:39:49 +0530

    Update main.js

--- desktop/main.js
+++ desktop/main.js
@@ -1,16 +1,16 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
-      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
-  win.loadFile(path.join(__dirname, 'renderer/index.html'));
+  win.loadFile("renderer/index.html");
 }
 
 app.whenReady().then(createWindow);
